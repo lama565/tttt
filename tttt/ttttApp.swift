e581bf07b72d6f43337006564019070989a937cd
Noura <queenfish360@gmail.com>
2024-09-26 16:51:55 +0300

    Initial Commit

---
+++ tttt/ttttApp.swift
@@ -0,0 +1,17 @@
+//
+//  ttttApp.swift
+//  tttt
+//
+//  Created by Noura Aldosari on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct ttttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
